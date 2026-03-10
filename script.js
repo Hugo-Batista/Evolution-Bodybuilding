c1c847e9f5990dac0564169b82c6f7fc1626bf7f
Hugo-Batista <hbtech.scv@gmail.com>
2026-03-10 20:05:09 -0300

    Alteração Index e Acionamento do arquivo Script (JavaScript)

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Ola,Mundo!")
+
+const inputEmail = document.getElementById("email-usuario")
+const inputSenha = document.getElementById("senha-usuario")
+const button = document.getElementById("button")
+
+
+
+button.addEventListener("click", () => { 
+    event.preventDefault(); // Isso impede que a página recarregue e limpe o console, pois o Botao esta denfto de um <Form/> no Index.
+const valorEmail = inputEmail.value
+const valorSenha = inputSenha.value
+
+if (valorEmail === "Hugobatista.89@hotmail.com" && valorSenha === "123456" ){
+
+    console.log("Acesso Permitido")
+}
+
+else{ console.log ("Acesso Negado")}
+
+});
+

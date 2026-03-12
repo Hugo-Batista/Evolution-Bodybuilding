8bb54fa65695b6e38a4e5c5dfe2b9e21ec2908e1
Hugo-Batista <hbtech.scv@gmail.com>
2026-03-12 18:08:36 -0300

    Acionamento dos arquivo dashboard.html, acionamento do arquivo dashboard.css e acionamento de melhprias nos arquivos index e script.

--- script.js
+++ script.js
@@ -2,20 +2,37 @@
 const inputEmail = document.getElementById("email-usuario")
 const inputSenha = document.getElementById("senha-usuario")
 const button = document.getElementById("button")
+const Valorparagrafo = document.getElementById("message")
 
 
 
-button.addEventListener("click", () => { 
+
+button.addEventListener("click", (event) => { 
     event.preventDefault(); // Isso impede que a página recarregue e limpe o console, pois o Botao esta denfto de um <Form/> no Index.
-const valorEmail = inputEmail.value
-const valorSenha = inputSenha.value
+const valorEmail = inputEmail.value;
+const valorSenha = inputSenha.value;
 
 if (valorEmail === "Hugobatista.89@hotmail.com" && valorSenha === "123456" ){
 
-    console.log("Acesso Permitido")
+Valorparagrafo.textContent = "Acesso permitido! Redirecionando...";
+Valorparagrafo.style.color = "green";
+
+setTimeout(() => {
+window.location.href = "dashboard.html";
+}, 1000);
+
+
+  }
+
+else{
+    Valorparagrafo.textContent = "E-mail ou senha Incorretos";
+    Valorparagrafo.style.color = "red";
 }
 
-else{ console.log ("Acesso Negado")}
+inputEmail.value = "";
+inputSenha.value = "";
+})
 
-});
+// === Esse é um operador de Comparação 
+// && Esse é um Operador Logico de Confirmação
 
